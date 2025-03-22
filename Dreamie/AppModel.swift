e86d670854c82ce520844bf3c28a6908ac4c1d73
dezdoesit <dez.blair06@gmail.com>
2025-03-22 14:27:33 -0400

    Initial Commit

---
+++ Dreamie/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Dreamie
+//
+//  Created by Dezmond Blair on 3/22/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
